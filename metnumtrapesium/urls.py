35acec478ac37080073e6f71bde4a63b8ad78829
muzakkiahsan <115060485+muzakkiahsan@users.noreply.github.com>
2025-06-11 18:24:13 +0700

    Update urls.py

--- metnumtrapesium/urls.py
+++ metnumtrapesium/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('trapesiumapp.urls')),  # pastikan nama app benar
+    path('', include('trapesiumapp.urls')),  # pastikan nama app benar
 ]
