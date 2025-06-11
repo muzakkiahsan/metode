01f713f8659cd9f0a4f1eb06ba5f2aaa2f5e4f2c
muzakkiahsan <115060485+muzakkiahsan@users.noreply.github.com>
2025-06-11 18:25:50 +0700

    Update settings.py

--- metnumtrapesium/settings.py
+++ metnumtrapesium/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rv0&%l$-jrkwihcks^m+=kepp(*ycm0t$%3ypan1zkwk0u7x(a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
