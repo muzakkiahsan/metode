6a1ede693b2c37a8b481e6a341b588cb6e70ceef
muzakkiahsan <115060485+muzakkiahsan@users.noreply.github.com>
2025-06-11 18:43:59 +0700

    Update urls.py

--- metnumtrapesium/urls.py
+++ metnumtrapesium/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView # Untuk melayani index.html
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('trapesiumapp.urls')),  # pastikan nama app benar
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
