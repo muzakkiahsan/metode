e73d0ff68048122ae97d46672a50a3365881cb81
muzakkiahsan <115060485+muzakkiahsan@users.noreply.github.com>
2025-06-11 20:51:18 +0700

    Update settings.py

--- metnumtrapesium/settings.py
+++ metnumtrapesium/settings.py
@@ -75,7 +75,7 @@ ROOT_URLCONF = 'metnumtrapesium.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [REACT_BUILD_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
